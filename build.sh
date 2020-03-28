909cf1ba051469387dd918cb66cef396d9ebbb26
Beinsezii <beinsezii@gmail.com>
2020-03-27 19:45:51 -0700

    Workflow Mostly Functional
    .svg's share assets, generate non-animated xcursor files,
    link cursors to the necessary places
    TODO: animation support. ideally make some per-cursor scripts to
    generate the svg frames. perhaps build.sh can call cursorname/anim.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,69 @@
 #!/usr/bin/bash
+
+# 64 * 0.25, 0.5, etc through 2.0
+# TODO make more customizable or something idk maybe this is a python job
+SIZES=(24 32 48 64 80 96 112 128)
+# argv1: svg name
+# argv2: hotspot. ul (uppperleft), up, ur, mid. fallback mid
+# args+: links
+# so `genstatic cursor_left ul arrow default` will
+# link the x cursors 'arrow' and 'default' with the
+# export of cursor_left.svg and a hotspot in the upper-left
+genstatic(){
+    echo -n > ./working/working.in
+
+
+    for s in ${SIZES[*]}
+    do
+        if [ $2 == ul ]
+        then
+            hotx=0
+            hoty=0
+        elif [ $2 == up ]
+        then
+            let hotx=$s/2
+            hoty=0
+        elif [ $2 == ur ]
+        then
+            hotx=$s
+            hoty=0
+        else
+            let hotx=$s/2
+            hoty=$hotx
+        fi
+
+        inkscape -w $s -h $s ./src/$1.svg -o ./working/$1_$s.png
+        echo $s $hotx $hoty ./working/$1_$s.png 100 >> ./working/working.in
+    done
+
+    xcursorgen ./working/working.in ./icons/nier_cursors/nier/$1
+
+    cd ./icons/nier_cursors/cursors/
+    for link in ${@:3}
+    do
+        ln -sf ../nier/$1 ./$link
+    done
+    cd ../../../
+
+}
+
+#same syntax as genstatic(), but looks for a folder of svgs to make an animtion
+genanim() {
+    echo TODO
+}
+
+
+for folder in ./working/ ./icons/ ./icons/nier_cursors/ ./icons/nier_cursors/nier/ ./icons/nier_cursors/cursors
+do
+    if [ ! -d $folder ]
+    then
+        mkdir $folder
+    fi
+done
+
+genstatic cursor_left ul left_ptr arrow default top_left_arrow
+
+# inherits Adwaita since that's standard-issue and should be a good fallback
+echo """[Icon Theme]
+Name=NieR Cursors
+Inherits=Adwaita""" > ./icons/nier_cursors/index.theme
